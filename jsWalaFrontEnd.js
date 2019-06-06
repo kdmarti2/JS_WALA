3774b013f085a2722929e4d4a2858e71f38e4476
kdmarti2 <kdmarti2@ncsu.edu>
2019-06-06 18:33:40 +0000

    oops minor rename

---
+++ jsWalaFrontEnd.js
@@ -0,0 +1,24 @@
+
+const esprima = require('esprima');
+const escodegen = require('escodgen');
+
+
+const { normalize } = require('./normalizer/lib/normalizer.js');
+
+//does not crash
+exports.ssa_transform = function (input) {
+    let results = {}
+    try {    
+        let ast = esprima.parseScript(input);
+        let ast2 = normalize(ast);
+        let src = escdoegen.generate(ast2);
+        results.error = "success"
+        results.msg = src;
+    } catch (err) {
+        results.error = "fail"
+        results.msg = err
+    } finally {
+        return results;
+    }
+
+}
